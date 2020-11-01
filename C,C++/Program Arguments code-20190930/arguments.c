3800dc211b2df11394dbf947007bfed8e607630f
AbhishekSinhaCoder <abhisheksinha1506@gmail.com>
2020-11-01 21:28:22 +0000

    A bit tidying up

--- C,C++/Program Arguments code-20190930/arguments.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-   int i;
-   for ( i=0; i<argc; i++ )
-      printf( "Parameter %d is: %s\n", i, argv[i] );
-}
--- C,C++/ddsamp/ReadMe.txt
+++
@@ -1,9 +0,0 @@
-
-Simple sample DirectDraw application
-
-Copyright (C) David Joffe 1999-2000
-
-http://www.geocities.com/SoHo/Lofts/2018/
-
-
-
